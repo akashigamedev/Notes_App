e84a504feba2ff2cd25dd53ae4075a4a408c51c1
Hiteshkuts <21hiteshkuts@gmail.com>
2024-10-19 18:44:35 +0530

    design

--- app/src/main/java/com/akashi/notesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/akashi/notesapp/presentation/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.colorResource
+import androidx.core.provider.FontsContractCompat.Columns
+import com.akashi.notesapp.presentation.components.Note
 import com.akashi.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotesAppTheme {
-
-            }
+                Column(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+Note()
+                    Note()
+Note()
+                    Note()
+                }
+                }
         }
     }
 }
---
+++ app/src/main/java/com/akashi/notesapp/presentation/components/Note.kt
@@ -0,0 +1,32 @@
+package com.akashi.notesapp.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Note(){
+    Column( ) {
+
+        Box(modifier = Modifier.size(120.dp).clip(RoundedCornerShape(10.dp)).background(Color.DarkGray).padding(16.dp)) {
+            Text("This is the description", color = Color.White, fontSize = 12.sp)
+
+        }
+        Text("Title", color = Color.White, fontSize = 12.sp,)
+        Text("Duration", color = Color.White, fontSize = 12.sp)
+        Text("Date", color = Color.White, fontSize = 12.sp)
+    }
+}
